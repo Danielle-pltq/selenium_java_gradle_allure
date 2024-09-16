9dd799551243691d2514f00db51f04990a5d025f
Danielle <danielle.petersen@polteq.com>
2024-09-16 08:58:43 +0200

    Ook wel handig om deze mee te committen

--- app/src/test/java/selenium_java_gradle_allure/CoffeeTest.java
+++ app/src/test/java/selenium_java_gradle_allure/CoffeeTest.java
@@ -1,31 +1,19 @@
 package selenium_java_gradle_allure;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import org.testng.Assert;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class CoffeeTest {
 
     WebDriver driver;
-    String Url = "https://coffee-cart.app/";
 
     @BeforeTest
     public void SetUp() {
-        // setting the driver executable
-        WebDriverManager.chromedriver().setup();
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-search-engine-choice-screen");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        driver = GenericTestHelpers.getHeadlessDriver("edge");
     }
 
     @Test
